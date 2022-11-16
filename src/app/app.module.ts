f6b17fe60219d10f4eaadb4afc7696217550ec13
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-11-16 12:33:00 +0100

    final commit part 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HttpClientModule } from "@angular/common/http";
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    OrdersComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/orders/orders.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.css']
+})
+export class OrdersComponent implements OnInit {
+  orders :any;
+  customerId!:number;
+  constructor(private http:HttpClient, private router: Router, private route:ActivatedRoute) {
+    this.customerId=route.snapshot.params['customerId'];
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8888/BILLING-SERVICE/bills/search/byCustomerId?customerId="+this.customerId+"&projection=fullOrder")
+      .subscribe({
+        next : (data)=>{
+          this.orders=data;
+          //console.log(this.customerId)
+          //console.log(this.orders)
+        },
+        error : (err)=>{}
+      });
+  }
+
+  getOrderDetails(o: any) {
+    this.router.navigateByUrl("/order-details/"+o.id);
+  }
+}
